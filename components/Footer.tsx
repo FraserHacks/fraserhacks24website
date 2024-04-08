03d3c4362bb92bf03aadd75d69451d4cea1f4884
Joshua Chou <joshuachou912@gmail.com>
2024-04-08 14:25:08 -0400

    Removed footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,8 +6,8 @@ import Image from "next/image"
 export default function Footer() {
     return (
         <footer className="flex flex-col items-center justify-center bg-black text-white py-4">
-            <hr className="flex h-px bg-white w-[100%] lg:w-[70%] my-5"></hr>
-            <div className="flex items-center gap-3">
+            {/* <hr className="flex h-px bg-white w-[100%] lg:w-[70%] my-5"></hr> */}
+            {/* <div className="flex items-center gap-3">
                 <a href="https://github.com/AritroSaha10" target="_blank">
                     <Image src="/socials/icon_clyde_blurple_RGB.svg" alt="Discord Link" width={27} height={27} id="discord" />
                 </a>
@@ -17,8 +17,8 @@ export default function Footer() {
                 <a href="https://github.com/AritroSaha10" target="_blank">
                     <Image src="/socials/icon_clyde_blurple_RGB.svg" alt="Discord Link" width={27} height={27} id="discord" />
                 </a>
-            </div>
-            <hr className="flex h-px bg-white w-[100%] lg:w-[70%] my-5"></hr>
+            </div> */}
+            {/* <hr className="flex h-px bg-white w-[100%] lg:w-[70%] my-5"></hr> */}
             <p className="text-sm">Made with ❤️ by the FraserHacks team</p>
         </footer>
     )
