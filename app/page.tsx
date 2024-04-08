5c18adc8e9497dbd70162ba3cd42c31876aadae7
Joshua Chou <joshuachou912@gmail.com>
2024-04-07 20:30:11 -0400

    Added short description (about)

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import Image from 'next/image';
-import styles from "../styles.module.css"
-
+import styles from "../styles.module.css";
 
 export default function Home() {
-
-
   return (
-    <main className="min-h-screen flex flex-col items-center justify-between p-24">
-    <div> 
+    <main className="min-h-screen flex flex-col items-center justify-start p-24 space-y-12">
+      <div> 
         <Image
           id="hero-logo"
           src="/hero-logo.png"
@@ -16,24 +13,13 @@ export default function Home() {
           height={500}
           className={`${styles.blink} ${styles.glow}`}
         />
-        
-     
-        
-          
-            <div className="text-center max-w-[800px] mx-auto" >
+      </div>
+      
+      <div className="text-center max-w-[800px] mx-auto">
         <h2 className={`${styles.arcadeStyle} text-white text-4xl font-semibold mt-4`}>May 4, 2019</h2>
         <h3 className="text-gray-400 text-xl font-normal mt-2 mb-5">John Fraser Secondary School, Mississauga</h3>
         <div id="mc_embed_signup">
-          <form
-            action="https://FraserHacks.us19.list-manage.com/subscribe/post?u=a50dd0c50b611bd5e179d9b85&amp;id=36ad7a17fc"
-            method="get"
-            className="validate"
-            id="mc-embedded-subscribe-form"
-            name="mc-embedded-subscribe-form"
-            target="_blank"
-            noValidate
-          >
-            <label>
+        <label>
               <span className="label">
                 Sign up for our mailing list to be notified of when applications are open.
               </span>
@@ -48,11 +34,40 @@ export default function Home() {
               <input type="text" name="b_a50dd0c50b611bd5e179d9b85_36ad7a17fc" value="" />
             </div>
             <div className="info"></div>
-          </form>
-          <div className="scroll-down"></div>
-          </div> 
+        
+        </div> 
+      </div>
+
+      {/* Short Description Section */}
+      <h2 className={`${styles.arcadeStyle} text-3xl text-white`}>Mississauga's Biggest Hackathon</h2>
+      <div className="max-w-[800px] mx-auto flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4">
+
+  <div className="w-full md:w-1/2">
+
+    <img src="/descript-img.png" alt="Description" className="w-full h-auto" />
+  </div>
+  <div className="w-full md:w-1/2">
+
+  <p className={`${styles.arcadeStyle} text-xl mt-4 leading-6 tracking-wide`}>
+  FraserHacks is Mississauga's largest high school hackathon organized entirely by students at John Fraser Secondary School. More than 100 students
+  <br></br>
+  <br></br>
+  2665 Erin Centre Blvd, Mississauga, ON L5M 5H6
+</p>
+
+  </div>
+</div>
+
+
+      {/* FAQ Section */}
+      <div className="max-w-[800px] mx-auto">
+        <h2 className="text-3xl text-white">FAQ</h2>
+        <div className="text-gray-300 mt-4">
+          {/* Your FAQ content here */}
         </div>
       </div>
+
+      {/* Additional sections can be added here following the same pattern */}
     </main>
   );
 }
