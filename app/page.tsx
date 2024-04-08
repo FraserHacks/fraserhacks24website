c78a5a9a6696c2f528e68bb52d7bce95c0d2340d
Joshua Chou <joshuachou912@gmail.com>
2024-04-08 11:31:33 -0400

    Refractoring: Description as seperate component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import styles from "../styles.module.css"
 import Faq from '../components/Faq';
-
+import Description from '../components/Description'
 
 export default function Home() {
   return (
@@ -40,39 +40,9 @@ export default function Home() {
         </div>
       </div>
 
-      {/* Short Description Section */}
-      <h2 className={`${styles.arcadeStyle} text-4xl text-white`}>Mississauga's Biggest Hackathon</h2>
-      <div className="max-w-[800px] mx-auto flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4">
+      <Description />
+     
 
-        <div className="w-full md:w-1/2">
-
-          <img src="/descript-img.png" alt="Description" className="w-full h-auto" />
-        </div>
-        <div className="w-full md:w-1/2">
-
-          <p className="text-xl mt-4 leading-6 tracking-wide">
-          FraserHacks is Mississauga's largest high school hackathon organized entirely by students at John Fraser Secondary School. More than 100 students from all of Peel Region will be coming together on May 4, 2019, for a full 10 hours of friendly hacking, enthusiastic learning, and delicious food.
-            <br></br>
-            <br></br>
-            <b>2665 Erin Centre Blvd, Mississauga, ON L5M 5H6</b>
-          </p>
-
-        </div>
-      </div>
-
-
-
-        <div className="w-full md:w-1/2">
-
-        <h2 className={`${styles.arcadeStyle} text-center text-4xl text-white`}>FraserHacks 2019</h2>
-          <p className="text-xl mt-4 leading-6 tracking-wide">
-            On May 4 2019 students from all over Peel gathered together to innovate, learn and meet new people. We're excited to bring the fun back this year with FraserHacks 2024. For a look back visit <a href="https://kev.lu/FraserHacks-Website/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700">FraserHacks 2019</a>.
-          </p>
-
-        </div>
-   
-  
-      {/* FAQ Section */}
       <Faq />
 
 
