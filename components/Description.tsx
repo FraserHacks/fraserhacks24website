8114c6fe910978d4e9ceea1ddcd6a85106978e8a
Joshua Chou <joshuachou912@gmail.com>
2024-04-08 13:34:33 -0400

    Unescaped elements

--- components/Description.tsx
+++ components/Description.tsx
@@ -1,4 +1,5 @@
 import styles from '../styles.module.css'
+import Image from 'next/image';
 
 export default function Description(){
 return (
@@ -8,12 +9,19 @@ return (
 
   <div className="w-full md:w-1/2">
 
-    <img src="/descript-img.png" alt="Description" className="w-full h-auto" />
+    <Image
+    id="description-img"
+     src="/descript-img.png" 
+     alt="Description"
+    className="w-full h-auto" 
+     width={500}
+     height={500}
+    />
   </div>
   <div className="w-full md:w-1/2">
 
     <p className="text-xl mt-4 leading-6 tracking-wide">
-    FraserHacks is Mississauga's largest high school hackathon organized entirely by students at John Fraser Secondary School. More than 100 students from all of Peel Region will be coming together on May 4, 2019, for a full 10 hours of friendly hacking, enthusiastic learning, and delicious food.
+    FraserHacks is Mississauga&apos;s largest high school hackathon organized entirely by students at John Fraser Secondary School. More than 100 students from all of Peel Region will be coming together on May 4, 2019, for a full 10 hours of friendly hacking, enthusiastic learning, and delicious food.
       <br></br>
       <br></br>
       <b>2665 Erin Centre Blvd, Mississauga, ON L5M 5H6</b>
@@ -28,7 +36,7 @@ return (
 
   <h2 className={`${styles.arcadeStyle} text-center text-4xl text-white mt-6`}>FraserHacks 2019</h2>
     <p className="text-xl mt-4 leading-6 tracking-wide">
-      On May 4 2019 students from all over Peel gathered together to innovate, learn and meet new people. We're excited to bring the fun back this year with FraserHacks 2024. For a look back visit <a href="https://kev.lu/FraserHacks-Website/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700">FraserHacks 2019</a>.
+      On May 4 2019 students from all over Peel gathered together to innovate, learn and meet new people. We&apos;re excited to bring the fun back this year with FraserHacks 2024. For a look back visit <a href="https://kev.lu/FraserHacks-Website/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700">FraserHacks 2019</a>.
     </p>
 
   </div>
