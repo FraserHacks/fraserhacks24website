6d74bfbd20bd77b9f418c89774d3f95b7a33f8dc
Joshua Chou <joshuachou912@gmail.com>
2024-04-10 20:20:22 -0400

    Added 2019 fraserhacks youtube video

--- components/Description.tsx
+++ components/Description.tsx
@@ -32,14 +32,20 @@ return (
 
 
 
+<div className="flex flex-col md:flex-row">
   <div className="w-full md:w-1/2">
 
-  <h2 className={`${styles.arcadeStyle} text-center text-4xl text-white mt-6`}>FraserHacks 2019</h2>
-    <p className="text-xl mt-4 leading-6 tracking-wide">
-      On May 4 2019 students from all over Peel gathered together to innovate, learn and meet new people. We&apos;re excited to bring the fun back this year with FraserHacks 2024. For a look back visit <a href="https://kev.lu/FraserHacks-Website/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700">FraserHacks 2019</a>.
-    </p>
-
+  <div className="w-full md:w-1/2 md:max-w-[560px] mx-auto">
+  <p className="text-xl mt-4 leading-6">
+    On May 4, 2019, students from all over Peel gathered together to innovate, learn, and meet new people. We're excited to bring the fun back this year with FraserHacks 2024. For a look back visit <a href="https://kev.lu/FraserHacks-Website/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700">FraserHacks 2019</a>.
+  </p>
+</div>
   </div>
+  <div className="w-full md:w-1/2 flex justify-center items-center">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/p5iQph5EPK4" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+  </div>
+</div>
+
   </section>
 )
 }
