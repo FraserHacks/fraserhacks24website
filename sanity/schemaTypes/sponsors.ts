e9e1244c3ca4153b0e4bfb0de89838377a4e0d51
Tony L <708162@pdsb.net>
2024-04-14 13:27:40 -0400

    feat: start adding sponsor infra

---
+++ sanity/schemaTypes/sponsors.ts
@@ -0,0 +1,72 @@
+import { MdAttachMoney } from "react-icons/md";
+
+export default {
+    name: 'sponsor',
+    title: 'Sponsors',
+    type: 'document',
+    icon: MdAttachMoney,
+    fields: [
+        {
+            name: "order",
+            title: "Order",
+            type: "number",
+            hidden: true
+        },
+        {
+            name: "name",
+            title: "Name",
+            type: "string"
+        },
+        {
+            name: "link",
+            title: "Link",
+            type: "string"
+        },
+        {
+            name: "tier",
+            title: "Tier",
+            type: "string",
+            options: {
+                list: [
+                    { title: 'Platinum / Hatchling', value: 'platinum' },
+                    { title: 'Gold / Juvenile', value: 'gold' },
+                    { title: 'Silver / Hatchling', value: 'silver' },
+                    { title: "Bronze / Egg", value: "bronze" },
+                    { title: "None", value: "none" }
+                ],
+                layout: 'radio'
+            }
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        },
+    ],
+    preview: {
+        select: {
+            title: 'name',
+            subtitle: 'tier',
+            media: 'image',
+        },
+    },
+    orderings: [
+        {
+            title: 'Order',
+            name: 'order',
+            by: [
+                { field: 'order', direction: 'asc' }
+            ]
+        },
+        {
+            title: 'Name',
+            name: 'name',
+            by: [
+                { field: 'name', direction: 'asc' }
+            ]
+        },
+    ]
+}
