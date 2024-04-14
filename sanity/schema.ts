82b83148f15c971dd2f5370032a3a0ff1bfc629c
Tony L <708162@pdsb.net>
2024-04-14 13:30:49 -0400

    Revert "feat: start adding sponsor infra"
    This reverts commit e9e1244c3ca4153b0e4bfb0de89838377a4e0d51.

--- sanity/schema.ts
+++
@@ -1,7 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-
-import sponsors from './schemaTypes/sponsors'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [sponsors],
-}
