521b65ba9c4fa293c36453228f13d8e7193a2ed7
Joshua Chou <joshuachou912@gmail.com>
2024-04-15 09:36:30 -0400

    Dropdown nav for mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,8 +7,14 @@ function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [navbarScrolled, setNavbarScrolled] = useState(false);
 
-  function handleClick() {
+  // Function to toggle the menu's open/close state
+  function toggleMenu() {
     setMenuOpen(!menuOpen);
+  }
+
+  // Function to close the menu, intended for link navigation
+  function closeMenu() {
+    setMenuOpen(false);
   }
 
   useEffect(() => {
@@ -22,36 +28,34 @@ function Navbar() {
   }, []);
 
   return (
-    <div className={`navbar-root-container fixed inset-x-0 top-0 z-10 justify-center items-center transition-all duration-300 w-full  `}>
-    <div
-      className={`navbar-container transition-all duration-300 mx-auto flex  h-16 lg:h-20 px-10 ${navbarScrolled ? 'max-w-5xl' : 'max-w-7xl'} ${navbarScrolled ? 'shadow-md bg-blue-800' : 'bg-blue-800'}`}
-    >
+    <div className={`navbar-root-container fixed inset-x-0 top-0 z-10 justify-center items-center transition-all duration-300 w-full`}>
+      <div className={`navbar-container transition-all duration-300 mx-auto flex h-16 lg:h-20 px-10 ${navbarScrolled ? 'max-w-5xl' : 'max-w-7xl'} ${navbarScrolled ? 'shadow-md bg-blue-800' : 'bg-blue-800'}`}>
         <div className='desktop-container flex justify-between w-full items-center h-full'>
-          <Link href="/"className="text-xl md:text-2xl lg:text-3xl font-bold hover:text-purple-400">
-       
-              Fraser Hacks
-       
+          <Link href="/" className="text-xl md:text-2xl lg:text-3xl font-bold hover:text-purple-400">
+            Fraser Hacks 
           </Link>
-  
-          <div className={`hidden lg:flex space-x-4 lg:space-x-8 ${navbarScrolled ? 'px-2 lg:px-4 max-w-4xl' : 'px-4 lg:px-8 max-w-6xl'} `}>
-            <NavbarLinks closeMobileMenu={handleClick} />
-          </div>
-  
-          <div className="lg:hidden">
-            <button onClick={handleClick} className="text-lg">
-              {menuOpen ? 'Close' : 'Menu'}
-            </button>
+
+          
+
+          <div className="relative lg:hidden">
+          <button onClick={toggleMenu} className="text-lg">
+            {menuOpen ? 'Close' : 'Menu'}
+          </button>
+          <div className={`absolute right-0 mt-2 bg-blue-800 shadow-md z-30 transition-opacity duration-300 ${menuOpen ? 'opacity-100 visible' : 'opacity-0 invisible'}`}>
+            <div className="flex flex-col items-start p-4">
+              <NavbarLinks closeMobileMenu={closeMenu} />
+            </div>
           </div>
         </div>
-      </div>
-  
-      <div className={`lg:hidden w-full ${menuOpen ? 'block' : 'hidden'}`}>
-        <NavbarLinks closeMobileMenu={handleClick} />
+
+        <div className={`hidden lg:flex space-x-4 lg:space-x-8`}>
+          <NavbarLinks closeMobileMenu={closeMenu} />
+        </div>
       </div>
     </div>
+    </div>
   );
-  
-  
 }
 
 export default Navbar;
+
